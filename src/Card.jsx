c58a50749a6a85d44ea600a152b3fbab39d61e57
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2024-10-24 23:10:10 -0400

    add ability to import images from folder and display all images on cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
-function Card() {
+function Card({ imageSrc, imageName }) {
     const [isClicked, changeClick] = useState(false);
 
     const onCardClick = () => {
-        changeClick(prevState => !prevState);
+        if (!isClicked){
+            changeClick(prevState => !prevState);
+        } else {
+            alert('You already clicked that one!');
+            changeClick(prevState => !prevState);
+        }
     }
 
     return (
-        <div onClick={onCardClick}>
-            <p>Card is {isClicked ? 'Clicked' : 'Not Clicked'} </p>
+        <div onClick={onCardClick} className="card">
+            <img className="cardImage" src={imageSrc} alt={imageName} style={{ width: '200px', height: 'auto' }} />
+            <p className="cardName">{imageName}</p>
         </div>
     )
+}
+
+Card.propTypes = {
+    imageSrc: PropTypes.string.isRequired,
+    imageName: PropTypes.string.isRequired,
 }
 
 export default Card
---
+++ src/trees.js
@@ -0,0 +1,12 @@
+const images = import.meta.glob('./images/*.{png,jpg,jpeg,svg}');
+
+const imageData = Object.keys(images).map((path) => {
+    const fileName = path.split('/').pop().split('.').slice(0, -1).join('.');
+  
+    return {
+      src: path.replace('./', '/src/'),
+      name: fileName,
+    };
+  });
+
+export default imageData;
